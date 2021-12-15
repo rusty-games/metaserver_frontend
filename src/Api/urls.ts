e911547949ffec53e641e667e4d7e7b23e971817
Jakub Królik <01142179@pw.edu.pl>
2021-12-15 19:34:54 +0100

    Changed admin urls

--- src/Api/urls.ts
+++ src/Api/urls.ts
@@ -4,5 +4,5 @@ export const games_url = BASE_URL + "/games/api/games/";
 export const games_accepted_url = BASE_URL + "/games/api/games/accepted/";
 export const games_not_accepted_url = BASE_URL + "/games/api/games/not-accepted/";
 export const rooms_url = BASE_URL + "/rooms/api/rooms/";
-export const login_url = BASE_URL + "/login/";
-export const logout_url = BASE_URL + "/logout/";
+export const login_url = BASE_URL + "/users/login/";
+export const logout_url = BASE_URL + "/users/logout/";
