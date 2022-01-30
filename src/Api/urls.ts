8fb19dd192b4171aa06cffca4bceeaca7104ac6c
Tomek Karwowski <to.karwowski@gmail.com>
2022-01-30 12:52:38 +0100

    env vars must start with 'REACT_APP_'

--- src/Api/urls.ts
+++ src/Api/urls.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.BACKEND_URL || "127.0.0.1:8080";
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL || "127.0.0.1:8080";
 export const BASE_HTTP_URL = "http://" + BASE_URL;
 
 export const websocket_channel_url = "ws://" + BASE_URL + "/ws/room/";
